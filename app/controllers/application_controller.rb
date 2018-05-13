cb535c82382bbe1afc7155795db24009ee7f6078
toshihide <get.enough.lactic.acid@gmail.com>
2018-05-13 13:09:32 +0900

    add partial view switching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_partial_view_display_condition
+
+  def set_partial_view_display_condition
+    controller = self.controller_name
+    action = self.action_name
+    if (controller == "registrations" && action == "new") ||
+       (controller == "sessions" && action == "new")
+      @display_condition = false
+    else
+      @display_condition = true
+    end
+  end
 end
