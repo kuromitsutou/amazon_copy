36d07b4b865fb0396c795d9d9cea798be52ce58a
toshihide <get.enough.lactic.acid@gmail.com>
2018-05-15 17:30:45 +0900

    create sigin view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,22 @@ class ApplicationController < ActionController::Base
 
   before_action :set_partial_view_display_condition
 
+
+  protected
+
   def set_partial_view_display_condition
     controller = self.controller_name
     action = self.action_name
     if (controller == "registrations" && action == "new") ||
-       (controller == "sessions" && action == "new")
+       (controller == "sessions" && action == "new") ||
+       (controller == "passwords" && action == "new")
       @display_condition = false
     else
       @display_condition = true
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana])
   end
 end
